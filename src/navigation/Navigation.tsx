a8eb149eba4e06466c49b7ced775ee9b2dccc621
Uzair Aslam <uzairaslam990@gmail.com>
2025-01-08 20:06:20 +0400

    feat:runing the project

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,16 +1,12 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
-import SplashScreen from './src/screens/splashScreen/SplashScreen'
 
-export class App extends Component {
+export default class Navigation extends Component {
   render() {
     return (
       <View>
         <Text> textInComponent </Text>
-        <SplashScreen/>
       </View>
     )
   }
 }
-
-export default App
